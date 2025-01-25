e4ae8de5b6121b20e69aa6c2e08749705ab83f46
Kajal-Gupta23 <kajal.gupta@probeinformation.com>
2025-01-26 04:32:50 +0530

    update settings

--- GoalTracker/backend/GoalTracker/settings.py
+++ GoalTracker/backend/GoalTracker/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'GoalTracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, config('DATABASE_NAME', default='db.sqlite3')),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
